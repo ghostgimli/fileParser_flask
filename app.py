f5724c20c75883c48b6f4c88d4882b2bad1dd1c4
pd.frolov <ghostgimli@gmail.com>
2024-03-04 17:09:24 +0300

    Changed algorithm of processing and exporting. No folders, only files

--- app.py
+++ app.py
@@ -37,13 +37,13 @@ def send_data():
             curdate = datetime.date.today().strftime('%d.%m.%Y')
             newfile_name = curdate + '_' + request.form['INC'] + '_' + request.form['AppEnv']
             try:
-                os.mkdir(app.config['UPLOAD_FOLDER']+'\\'+ newfile_name)
+                os.mkdir(app.config['UPLOAD_FOLDER']+'/'+ newfile_name)
             except (FileExistsError):
                 print('Folder already exists, skipping')
             file.save(os.path.join(app.config['UPLOAD_FOLDER'],  newfile_name+'.xml')) # cначала папка потом файл
             #return redirect(url_for('download_file', name=filename))
 
-            xml = XMLdoc(app.config['UPLOAD_FOLDER']+'\\'+ newfile_name+'.xml', request.form['Status'],request.form['EgrulNotIncluded'])
+            xml = XMLdoc(app.config['UPLOAD_FOLDER']+'/'+ newfile_name+'.xml', request.form['Status'],request.form['EgrulNotIncluded'])
             if xml.check_encoding()['encoding'] != 'utf-8':
                 xml.convert_encoding(old_encoding="iso-8859-5", new_encoding="utf-8")
 
