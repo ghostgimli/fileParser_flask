2b766e305dd8180b531a6d5ead73bfd854ad5515
pd.frolov <ghostgimli@gmail.com>
2024-03-04 17:25:53 +0300

    return folders

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def send_data():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], newfile_name, newfile_name+'.xml')) # cначала папка потом файл
             #return redirect(url_for('download_file', name=filename))
 
-            xml = XMLdoc(app.config['UPLOAD_FOLDER']+'/'+newfile_name+'/'+ newfile_name+'.xml', request.form['Status'],request.form['EgrulNotIncluded'])
+            xml = XMLdoc(app.config['UPLOAD_FOLDER']+'//'+newfile_name+'//'+ newfile_name+'.xml', request.form['Status'],request.form['EgrulNotIncluded'])
             if xml.check_encoding()['encoding'] != 'utf-8':
                 xml.convert_encoding(old_encoding="iso-8859-5", new_encoding="utf-8")
 
